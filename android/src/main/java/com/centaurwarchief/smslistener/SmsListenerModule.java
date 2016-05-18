0a859b95916773d88f9d954cd21d23806129dbbe
happyming9527 <happyming9527@gmail.com>
2016-05-18 23:32:04 +0800

    Update SmsListenerModule.java
    修复内存泄漏的bug

--- android/src/main/java/com/centaurwarchief/smslistener/SmsListenerModule.java
+++ android/src/main/java/com/centaurwarchief/smslistener/SmsListenerModule.java
@@ -5,37 +5,56 @@ import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.provider.Telephony;
-import com.facebook.react.bridge.LifecycleEventListener;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.LifecycleEventListener;
 
 public class SmsListenerModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     private BroadcastReceiver mReceiver;
     private Activity mActivity;
+    private String needRegister;
 
     public SmsListenerModule(ReactApplicationContext context, Activity activity) {
         super(context);
 
         mActivity = activity;
         mReceiver = new SmsReceiver(context);
+    }
 
+    @ReactMethod
+    public void init() {
+        needRegister = "yes";
         registerReceiverIfNecessary(mReceiver);
+    }
+
+    @ReactMethod
+    public void removeListener() {
+        needRegister = "no";
+        unregisterReceiver(mReceiver);
     }
 
     private void registerReceiverIfNecessary(BroadcastReceiver receiver) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            mActivity.registerReceiver(
-                receiver,
-                new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
-            );
+            if (needRegister == "yes") {
+                mActivity.registerReceiver(
+                        receiver,
+                        new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
+                );
+                return;
+            }
 
-            return;
         }
+        if (needRegister == "yes") {
 
-        mActivity.registerReceiver(
-            receiver,
-            new IntentFilter("android.provider.Telephony.SMS_RECEIVED")
-        );
+            mActivity.registerReceiver(
+                    receiver,
+                    new IntentFilter("android.provider.Telephony.SMS_RECEIVED")
+            );
+        }
     }
 
     private void unregisterReceiver(BroadcastReceiver receiver) {
@@ -61,4 +80,5 @@ public class SmsListenerModule extends ReactContextBaseJavaModule implements Lif
     public String getName() {
         return "SmsListener";
     }
+
 }
