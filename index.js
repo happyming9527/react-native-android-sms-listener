44958a0b8ffe111ed8def3c2c7695cdf4f6f9a69
happyming9527 <happyming9527@gmail.com>
2016-05-18 23:33:05 +0800

    Update index.js
    修复内存泄露的bug

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 /* @flow */
 import type CancellableSubscription from './CancellableSubscription'
 import type ReceivedSmsMessage from './ReceivedSmsMessage'
-import { DeviceEventEmitter } from 'react-native'
+import { NativeModules, DeviceEventEmitter } from 'react-native'
+const nativeModule = NativeModules.SmsListener;
 
 const SMS_RECEIVED_EVENT = 'com.centaurwarchief.smslistener:smsReceived'
 
 export default {
-  addListener(
-    listener: (message: ReceivedSmsMessage) => void
-  ): CancellableSubscription {
+  addListener(listener: (message: ReceivedSmsMessage) => void ): CancellableSubscription {
     return DeviceEventEmitter.addListener(
       SMS_RECEIVED_EVENT,
       listener
     )
+  },
+  init: ()=>{
+    nativeModule.init()
+  },
+  removeListener: ()=>{
+    nativeModule.removeListener()
   }
 }
